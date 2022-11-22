56e46278e7bd03d41b63b91f9fb48ce6057b2873
William Patrick <117779516+dimitri8421@users.noreply.github.com>
2022-11-21 22:24:28 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const tempo = setInterval(async()=> {
     if (window.screen.height > window.screen.width) {
         if (!document.querySelector('[class="swal2-container swal2-center swal2-backdrop-show"]')) {
-            document.querySelector('body').style.display = "none";
+            document.querySelector('.main').style.display = "none";
             Swal.fire({
                 html: '<div style="display:flex;align-items: baseline;margin: 1em;"><div style="font-size: 1.5em;font-family: sans-serif;">Por favor gire a tela do celular</div><img src="img/girar-tela.png" style="height: 34px;padding: 0 8px;"></div>',
                 showConfirmButton: false
@@ -9,7 +9,7 @@ const tempo = setInterval(async()=> {
         }
 
     } else {
-        document.querySelector('body').style.display = "";
+        document.querySelector('.main').style.display = "";
     }
 },500);
 
